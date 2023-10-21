b2baf18dc94b880709b290578c4759bc3ac3c7a0
Antoine MLD <antoine.moulard@hotmail.com>
2023-10-21 17:46:40 +0200

    modification syntaxe

--- create_trigger.sql
+++ create_trigger.sql
@@ -1,9 +1,8 @@
-# Création d'un déclencheur BEFORE INSERT sur la table bmi
+-- Création d'un déclencheur BEFORE INSERT sur la table bmi
 CREATE TRIGGER IF NOT EXISTS trigger_bmi_association
 BEFORE INSERT ON bmi
 FOR EACH ROW
 BEGIN
-
-#Rechercher l'utilisateur associé dans la table user
-SELECT user_id INTO NEW.user_id FROM user WHERE username = NEW.username;
+    -- Rechercher l'utilisateur associé dans la table user
+    SELECT user_id INTO NEW.user_id FROM user WHERE username = NEW.username;
 END;
