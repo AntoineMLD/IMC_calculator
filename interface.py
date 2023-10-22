c8576079a3ccf5dd7b3613f6d390fb2b701397f6
Antoine MLD <antoine.moulard@hotmail.com>
2023-10-22 13:23:32 +0200

    Code fonctionnel avec tkinter, sans liaison avec la bdd

--- interface.py
+++ interface.py
@@ -1,102 +1,79 @@
 from schema import User, engine, Bmi, History
 from sqlalchemy.orm import sessionmaker
 
+# Fonction pour enregistrer les données de l'utilisateur
+def enregistrer_donnees_utilisateur(username, first_name, last_name, email, telephone, adresse, poids, taille):
+        user = User(username=username, first_name=first_name, last_name=last_name, adresse=adresse, telephone=telephone, email=email)
 
-# Nom de l'utilisateur :
-username = input("Veuillez saisir votre nom d'utilisateur : ")
-
-# L'utilisateur saisit son prénom :
-first_name = input("Renseignez votre prénom : ")
-
-# L'utilisateur saisit son nom:
-last_name = input("Renseignez votre nom : ")
-
-# L'utilisateur saisit son email:
-email = input("Renseignez votre mail : ")
-
-# L'utilisateur saisit son téléphone :
-telephone = input("Renseigner votre numéro de téléphone : ")
-
-# L'utilisateur saisit son adresse:
-adresse = input("Renseignez votre adresse postale : ")
-
-# L'utilisateur saisit son poids (en kg)
-poids = int(input("Veuillez saisir votre poids (en Kg) : ")) 
-
-# l'utilisateur saisit sa taille
-taille = float(input("Veuillez saisir votre taille (en cm) : "))
-taille = taille / 100 
-
-   
+        
 
 # Formule de l'imc
 def imc(poids,taille):
-        
-        # Formule de l'IMC
-        imc_resultat= round(poids / (taille * taille), 1)
-        return imc_resultat
-
-
-# Résultat de la fonction imc
-imc_resultat = imc(poids,taille) 
-
-
-# Fonction pour afficher la catégorisation de imc_resultat
-def affichage(imc):
-    categories_imc = {
-        (40, 100): "Obésité morbide (classe III). Attention risque cardio-vasculaire extrêmement élevé",
-        (35, 40): "Obésité sévère (classe II). Attention risque cardio-vasculaire très élevé !",
-        (30, 35): "Obésité modérée (classe I). Attention risque cardio-vasculaire élevé !",
-        (25, 30): "Surpoids ou pré-obésité. Attention risque cardio-vasculaire accru !",
-        (18.5, 25): "Corpulence normale. Risque cardio-vasculaire faible !",
-        (16.5, 18.5): "Maigreur. Attention risque cardio-vasculaire accru !",
-        (-100, 16.5): "Maigreur extrême - dénutrition. Attention risque cardio-vasculaire élevé !"
-    }
-
-    for (min_range, max_range), category in categories_imc.items():
-        if min_range < imc_resultat < max_range:
-            print(f"Votre IMC est de {imc_resultat}, {category}")
-
-
+     taille_m = taille / 100
+     imc_resultat = round(poids / (taille_m * taille_m), 1)
+     return imc_resultat
 
 # Créer la session SQLAlchemy
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Récupère l'IMC précedent de l'utilisateur s'il existe
-previous_bmi = None
 
-existing_user = session.query(User).filter_by(username=username).first()
-if existing_user:
-     previous_bmi = existing_user.bmi.imc_resultat
-else:
-     previous_bmi = None
+if __name__ == "__main__":
+
+    
+        username = input("Veuillez saisir votre nom d'utilisateur : ")
+        first_name = input("Renseignez votre prénom : ")
+        last_name = input("Renseignez votre nom : ")
+        email = input("Renseignez votre mail : ")
+        telephone = input("Renseigner votre numéro de téléphone : ")
+        adresse = input("Renseignez votre adresse postale : ")
+        poids = int(input("Veuillez saisir votre poids (en Kg) : ")) 
+        taille = float(input("Veuillez saisir votre taille (en cm) : "))
+        taille = taille / 100 
+
+        user = enregistrer_donnees_utilisateur(username, first_name, last_name, email, telephone, adresse, poids, taille)
+
+        # Résultat de la fonction imc
+        imc_resultat = imc(poids,taille) 
+
+        # Récupère l'IMC précedent de l'utilisateur s'il existe
+        previous_bmi = None
+
+        existing_user = session.query(User).filter_by(username=username).first()
+        if existing_user:
+            previous_bmi = existing_user.bmi.imc_resultat
+        else:
+            previous_bmi = None
+
+        # Récupère l'IMC précédent de l'utilisateur s'il existe
+        previous_bmi = None
+        existing_user = session.query(User).filter_by(username=username).first()
+        if existing_user:
+            previous_bmi = existing_user.bmi.imc_resultat
+
+        # Créer les objets user,bmi et history
+        user = User(username=username, first_name=first_name, last_name=last_name, adresse=adresse, telephone=telephone, email=email)
+
+        bmi = Bmi(poids=poids, taille=taille, imc_resultat=imc(poids, taille))
+        user.bmi = bmi
+
+        history = History(user_id=user.user_id, previous_bmi=previous_bmi, current_bmi=bmi.imc_resultat)
 
 
-# Crée les objets user, bmi et history
-user = User(username=username, first_name=first_name, last_name=last_name, adresse=adresse, telephone=telephone, email=email)
 
-bmi = Bmi(poids=poids, taille=taille, imc_resultat=imc_resultat)
-user.bmi = bmi
+        # Ajoute les objets à la session et commit
+        session.add(user)
+        session.add(bmi)
+        session.add(history)
+        session.commit()
 
-history = History(user_id=user.user_id, previous_bmi=previous_bmi, current_bmi=imc_resultat)
+ 
 
+    
 
-
-
-
-# Ajoute les objets à la session et commit
-session.add(user)
-session.add(bmi)
-session.add(history)
-session.commit()
+    
 
 
 
-
-
-imc(poids, taille)
-affichage(imc)
-
-
+    
 
--- ui.py
+++ ui.py
@@ -1,10 +1,124 @@
-from tkinter import Tk, Canvas
+import tkinter as tk
+from tkinter import font
+import interface
 
-WIDTH = 600
-HEIGHT = 400
 
-root = Tk()
-cnv = Canvas(root, width=WIDTH, height=HEIGHT, background='ivory')
-cnv.pcack()
+
+# Paramètres fenêtre
+root = tk.Tk()
+root.config(bg='ivory') 
+
+# Police Matrix
+matrix_font = font.Font(family='Courier', size=18)
+
+# Canvas animations
+canvas = tk.Canvas(root, bg='grey', highlightthickness=0)
+canvas.pack(fill='both', expand=True)
+
+
+# Labels
+label_username = tk.Label(root, text="Nom d'utilisateur", bg="ivory", fg="grey", font=matrix_font)
+label_nom = tk.Label(root, text="Nom :", bg="ivory", fg="grey", font=matrix_font)
+label_prenom = tk.Label(root, text="Prénom :", bg="ivory", fg="grey", font=matrix_font) 
+label_email = tk.Label(root, text="Email :", bg="ivory", fg="grey", font=matrix_font)
+label_telephone = tk.Label(root, text="Téléphone :", bg="ivory", fg="grey", font=matrix_font)
+label_adresse = tk.Label(root, text="Adresse :", bg="ivory", fg="grey", font=matrix_font)
+label_poids = tk.Label(root, text="Poids (kg) :", bg="ivory", fg="grey", font=matrix_font)
+label_taille = tk.Label(root, text="Taille (cm) :", bg="ivory", fg="grey", font=matrix_font)
+label_resultat = tk.Label(root, text="", bg="ivory", fg="grey", font=matrix_font)
+label_categories_imc = tk.Label(root, text="", bg="ivory", fg="grey", font=matrix_font)
+
+# Champs de saisie
+input_username = tk.Entry(root, bg="ivory", fg="grey", insertbackground="grey")
+input_nom = tk.Entry(root, bg="ivory", fg="grey", insertbackground="grey")
+input_prenom = tk.Entry(root, bg="ivory", fg="grey", insertbackground="grey")
+input_email = tk.Entry(root, bg="ivory", fg="grey", insertbackground="grey")
+input_telephone = tk.Entry(root, bg="ivory", fg="grey", insertbackground="grey")
+input_adresse = tk.Entry(root, bg="ivory", fg="grey", insertbackground="grey")
+input_poids = tk.Entry(root, bg="ivory", fg="grey", insertbackground="grey")
+input_taille = tk.Entry(root, bg="ivory", fg="grey", insertbackground="grey")
+
+
+# Fonction de calcul IMC
+def calculer_imc():
+    username = input_username.get()
+    first_name = input_nom.get()
+    last_name = input_prenom.get()
+    email = input_email.get()
+    telephone = input_telephone.get()
+    adresse = input_adresse.get()
+    poids = float(input_poids.get())
+    taille = float(input_taille.get())
+    interface.enregistrer_donnees_utilisateur(username, first_name, last_name, email, telephone, adresse, poids, taille)
+    
+    # calcul de l'imc
+    imc_resultat = interface.imc(poids,taille)
+
+
+    # Fonction pour afficher la catégorisation de imc_resultat
+    def affichage(imc_resultat):
+        categories_imc = {
+            (40, 100): "Obésité morbide (classe III). Attention risque cardio-vasculaire extrêmement élevé",
+            (35, 40): "Obésité sévère (classe II). Attention risque cardio-vasculaire très élevé !",
+            (30, 35): "Obésité modérée (classe I). Attention risque cardio-vasculaire élevé !",
+            (25, 30): "Surpoids ou pré-obésité. Attention risque cardio-vasculaire accru !",
+            (18.5, 25): "Corpulence normale. Risque cardio-vasculaire faible !",
+            (16.5, 18.5): "Maigreur. Attention risque cardio-vasculaire accru !",
+            (-100, 16.5): "Maigreur extrême - dénutrition. Attention risque cardio-vasculaire élevé !"
+    }
+
+        for (min_range, max_range), category in categories_imc.items():
+            if min_range <= imc_resultat <= max_range:
+                return category
+
+    
+    
+    # Label du résultat
+    result_text = f"IMC de {last_name} {first_name} : {imc_resultat}"
+    label_resultat["text"] = result_text
+    label_categories_imc["text"] = f"Catégorie IMC : {affichage(imc_resultat)}"
+    
+    
+# Bouton Calculer
+btn_calculer = tk.Button(root, text="Calculer", bg="ivory", fg="grey", command=calculer_imc) 
+
+
+# Placement des widgets
+
+label_username.pack()
+input_username.pack()
+
+label_nom.pack()
+input_nom.pack()
+
+label_prenom.pack()
+input_prenom.pack()
+
+label_email.pack()
+input_email.pack()
+
+label_telephone.pack()
+input_telephone.pack()
+
+label_adresse.pack()
+input_adresse.pack()
+
+label_poids.pack()
+input_poids.pack()
+
+label_taille.pack() 
+input_taille.pack()
+
+btn_calculer.pack(pady=20)
+label_resultat.pack()
+
+label_categories_imc.pack()
+label_resultat.pack()
+
+
+# Bouton quitter
+btn_quitter = tk.Button(root, text="Quitter", bg="ivory", fg="grey", command=root.quit)
+btn_quitter.pack(pady=20)
+
 
 root.mainloop()
